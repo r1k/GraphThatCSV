13427b211e9f472135c58f9da254c7c27a75ca22
Kelsey <r.i.kelsey@gmail.com>
2016-05-25 22:51:17 +0100

    Read add data selection

--- js/main.js
+++ js/main.js
@@ -74,6 +74,98 @@ function drawDashboard(csvdata, chartType, controlType) {
     }
   });
 
+  // create columns array
+  var columns = [0];
+  /* the series map is an array of data series
+   * "column" is the index of the data column to use for the series
+   * "roleColumns" is an array of column indices corresponding to columns with roles that are associated with this data series
+   * "display" is a boolean, set to true to make the series visible on the initial draw
+   */
+  var seriesMap = [];
+
+  for (var i = 1; i < csvdata[0].length; i++) {
+    var _display = false;
+    if (i < 2) {
+        _display = true;
+    }
+    seriesMap.push({
+    column: i,
+    display: _display
+    });
+  }
+  var columnsMap = {};
+  var series = [];
+  for (var i = 0; i < seriesMap.length; i++) {
+    var col = seriesMap[i].column;
+    columnsMap[col] = i;
+    // set the default series option
+    series[i] = {};
+    if (seriesMap[i].display) {
+        // if the column is the domain column or in the default list, display the series
+        columns.push(col);
+    }
+    else {
+      // otherwise, hide it
+      columns.push({
+        label: data.getColumnLabel(col),
+        type: data.getColumnType(col),
+        sourceColumn: col,
+        calc: function () {
+            return null;
+        }
+      });
+      // backup the default color (if set)
+      if (typeof(series[i].color) !== 'undefined') {
+        series[i].backupColor = series[i].color;
+      }
+      series[i].color = '#CCCCCC';
+    }
+  }
+  chart.setOption('series', series);
+
+  function showHideSeries () {
+    var sel = chart.getChart().getSelection();
+    // if selection length is 0, we deselected an element
+    if (sel.length > 0) {
+      // if row is undefined, we clicked on the legend
+      if (sel[0].row == null) {
+        var col = sel[0].column;
+        if (typeof(columns[col]) == 'number') {
+          var src = columns[col];
+
+          // hide the data series
+          columns[col] = {
+            label: data.getColumnLabel(src),
+            type: data.getColumnType(src),
+            sourceColumn: src,
+            calc: function () {
+              return null;
+            }
+          };
+
+          // grey out the legend entry
+          series[columnsMap[src]].color = '#CCCCCC';
+        }
+        else {
+          var src = columns[col].sourceColumn;
+
+          // show the data series
+          columns[col] = src;
+          series[columnsMap[src]].color = null;
+        }
+        var view = chart.getView() || {};
+        view.columns = columns;
+        chart.setView(view);
+        chart.draw();
+      }
+    }
+  }
+  
+  google.visualization.events.addListener(chart, 'select', showHideSeries);
+
+  var view = {columns: columns};
+  chart.setView(view);
+
   dash.bind(control, chart);
   dash.draw(data);
 }
